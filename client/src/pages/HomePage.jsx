1b330ad377aee359c26ef4cf9a4fd60a9e165b96
Eric <ekellogg90@gmail.com>
2024-09-03 21:08:41 -0500

    added bootstrap to package json

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
     return (        
     <>
         <div className="container pt-4">
-            <h1 className="text-center mb-2 mt-2 text-red-500">
+            <h1 className="text-center mb-2 mt-2 text-primary">
                 Welcome to JEB Jack
             </h1>
         </div>
