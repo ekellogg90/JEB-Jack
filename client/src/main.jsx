8431983d494240ee710fe097203c3e283d9022b7
Eric <ekellogg90@gmail.com>
2024-09-03 20:35:44 -0500

    added in homepage and tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,27 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import HomePage from './pages/HomePage.jsx';
+// import ProfilePage from './pages/ProfilePage.jsx';
+
+import './index.css';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <h1 className=''>Wrong page!</h1>,
     children: [
       {
         index: true,
-        // element: < />
+        element: <HomePage />
       }, 
+      // {
+      //   path: 'profile',
+      //   element: <ProfilePage />,
+      // }
     ]
   }
 ])
