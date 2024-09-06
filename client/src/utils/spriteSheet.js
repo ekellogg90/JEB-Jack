7931f3607649d10d21e9d8a3faadd70d95758cc7
Ben Hill <benjaminjhill@icloud.com>
2024-09-05 21:24:06 -0500

    imported and added function to resolve deckPromises in Cardjsx

--- client/src/utils/spriteSheet.js
+++ client/src/utils/spriteSheet.js
@@ -1,5 +1,5 @@
 //when ss is used it means "sprite sheet"
-function spriteSheetSlicer(ssWidth, ssHeight, ssRows, ssCols, filePath, skipNum) {
+async function spriteSheetCardCutter(ssWidth, ssHeight, ssRows, ssCols, filePath, skipNum) {
     const cardWidth = Math.floor((ssWidth - (ssCols + 1)) / ssCols); // 1 is the 1px boarder on all the sprites
     const cardHeight = Math.floor((ssHeight - (ssRows + 1)) / ssRows); // same thing with 1 here
 
@@ -9,37 +9,39 @@ function spriteSheetSlicer(ssWidth, ssHeight, ssRows, ssCols, filePath, skipNum)
     const ss = new Image();
     ss.src = filePath;
 
-    const cards = [];
+    return new Promise((resolve) => {
+        const cards = [];
 
-    ss.onLoad = () => {
-        for (let row = 0; row < ssRows; row++) {
-            for (let col = 0; col< ssCols; col++) {
-                if (row === ssRows - 1 && col >= ssCols - skipNum) continue; // skips the last skipNum spaces because they are blank
+        ss.onload = () => {
+            for (let row = 0; row < ssRows; row++) {
+                for (let col = 0; col< ssCols; col++) {
+                    if (row === ssRows - 1 && col >= ssCols - skipNum) continue; // skips the last skipNum spaces because they are blank
 
-                //cord calculation for each sprite
-                const x = col * (cardWidth + 1) + 1; //1 for 1px boarder
-                const y = row * (cardHeight + 1) + 1;
+                    //cord calculation for each sprite
+                    const x = col * (cardWidth + 1) + 1; //1 for 1px boarder
+                    const y = row * (cardHeight + 1) + 1;
 
-                //drawing
-                canvas.width = cardWidth;
-                canvas.height = cardHeight;
-                context.drawImage(ss, x, y, cardWidth, cardHeight, 0, 0, cardWidth, cardHeight);
+                    //drawing
+                    canvas.width = cardWidth;
+                    canvas.height = cardHeight;
+                    context.drawImage(ss, x, y, cardWidth, cardHeight, 0, 0, cardWidth, cardHeight);
 
-                //storing
-                const cardDataUrl = canvas.toDataURL();
-                cards.push(cardDataUrl);
+                    //storing
+                    const cardDataUrl = canvas.toDataURL();
+                    cards.push(cardDataUrl);
 
+                }
             }
-        }
-        console.log(cards);
-    }
+            resolve(cards);
+        };
+    });
 }
 
-const regularDeck = spriteSheetSlicer(5020, 5096, 7, 10, '../assets/sprite_sheets/regularDeck.png', 4);
-const specialDeck1 = spriteSheetSlicer(4016, 4368, 6, 8, '../assets/sprite_sheets/specialDeck1.png', 7);
-const specialDeck2 = spriteSheetSlicer(2008, 2184, 3, 4, '../assets/sprite_sheets/specialDeck2.png', 1);
+export const regularDeckPromise = spriteSheetCardCutter(5020, 5096, 7, 10, '../assets/sprite_sheets/regularDeck.png', 4);
+export const specialDeck1Promise = spriteSheetCardCutter(4016, 4368, 6, 8, '../assets/sprite_sheets/specialDeck1.png', 7);
+export const specialDeck2Promise = spriteSheetCardCutter(2008, 2184, 3, 4, '../assets/sprite_sheets/specialDeck2.png', 1);
 
-export default { regularDeck, specialDeck1, specialDeck2 };
+export default { regularDeckPromise, specialDeck1Promise, specialDeck2Promise };
 
 /* import will look like
 
