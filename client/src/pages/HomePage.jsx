415cd054315fba194602079c474c65be45116e5e
Eric <ekellogg90@gmail.com>
2024-09-05 21:17:52 -0500

    added game board iamge

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,25 @@
+import { Button } from 'react-bootstrap';
+import bjTable from '../assets/bjtable.jpg';
+
 export default function HomePage() {
+    const cardStyle = {
+        width: '100%',
+        height: '85vh',
+        backgroundImage: `url(${bjTable})`,
+        backgroundSize: "100% 100%",
+    };
     return (        
     <>
-        <div className="container pt-4">
+        <div className="container">
             <h1 className="text-center mb-2 mt-2 text-primary">
                 Welcome to JEB Jack
             </h1>
+
+            <div className="card align-items-center justify-content-center" style={cardStyle}>
+                <Button type='button' variant='danger'>
+                    PLAY GAME
+                </Button>
+            </div>
         </div>
     </>
     );
