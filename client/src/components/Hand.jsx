72813dfbd8a084781ccbaf4a5a62e892ee8e3dac
JohnKihm <john.a.kihm@gmail.com>
2024-09-11 12:41:35 -0500

    added logic for dealing cards at the start of the game

--- client/src/components/Hand.jsx
+++ client/src/components/Hand.jsx
@@ -7,11 +7,14 @@ export default function regularHand({cards,owner,handValue}) {
     <div>
         <h2>{owner}:{handValue}</h2>
         <div>
-            {cardIMGarr.map((card, index) => (
+            {/* {cardIMGarr.map((card, index) => (
             <Card key={index}
             deckName={card.deckName}
             cardIndex={card.cardIndex}
             />
+            ))} */}
+            {cards.map((card, index) => (
+                <div key={index}>{card.card}:{card.valueOfCard}</div>
             ))}
         </div>
     </div>
--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -15,9 +15,6 @@ export default function Game() {
     const [dealerHandValue, setDealerHandValue] = useState(0);
     const [dealerCanHit, setDealerCanHit] = useState(true);
 
-    // let dealerHand = [];
-    // let dealerHandValue = 0;
-
     const [gameOver, setGameOver] = useState(false);
     const [result, setResult] = useState({type: "", message: ""});
     const [newGame, setNewGame] = useState(false);
@@ -26,9 +23,15 @@ export default function Game() {
     "dark magician", "abomb", "goojf", "babe ruth"];
 
     //get random card and remove it from the deck and update the state
-    const getRandomCard = () => {
-        const randIndex = Math.floor(Math.random() * gameDeck.length);
-        const card = gameDeck[randIndex]; 
+    const getRandomCard = (noSpecials) => {
+        let randIndex = Math.floor(Math.random() * gameDeck.length);
+        let card = gameDeck[randIndex];
+        if (noSpecials) {
+            while (alternateHandCards.some(substring => card.valueOfCard.includes(substring))) {
+                randIndex = Math.floor(Math.random() * gameDeck.length);
+                card = gameDeck[randIndex];
+            }
+        }
         const updatedDeck = gameDeck.filter((_, index) => index !== randIndex);
 
         //deck update
@@ -38,12 +41,40 @@ export default function Game() {
         return card;
     };
 
+    const gameStart = () => {
+        const playerCards = [getRandomCard(true), getRandomCard(true)];
+        let dealerCard = getRandomCard(true);
+
+        if (dealerCard.card === 'tarot') {
+            setDealerCanHit(false);
+        }
+
+        // Checks for special cards to send to the player's special hand
+        playerCards.map((card) => {
+            if (card.card === 'tarot') {
+                setPlayerCanHit(false);
+            }
+        });
+
+        // Get each hand's score
+        const newPlayerValue = calcHandValue(playerCards);
+        const newDealerValue = calcHandValue([dealerCard]);
+        
+        // Set all state variables
+        setPlayerHand(playerCards);
+        setDealerHand([dealerCard]);
+        setPlayerHandValue(newPlayerValue);
+        setDealerHandValue(newDealerValue);
+    };
+
     const dealCardToPlayer = () => {
         // const gameSavingCards = ["charizard", "dark magician", "abomb", "goojf", "babe ruth"];
 
-        const card = getRandomCard();
+        const card = getRandomCard(false);
         console.log(card);
-        if (alternateHandCards.some(substring => card.valueOfCard.includes(substring))) { // needs work still
+
+        // Checks for special cards and sends them to the special hand
+        if (alternateHandCards.some(substring => card.valueOfCard.includes(substring))) {
             const newSpecialHand = [...playerSpecialHand, card];
             setPlayerSpecialHand(newSpecialHand);
         } else {
@@ -51,30 +82,16 @@ export default function Game() {
             setPlayerHand(newJJHand);
             const newHandValue = calcHandValue(newJJHand)
             setPlayerHandValue(newHandValue);
-            //console.log(newJJHand);
-            //console.log(playerHandValue);
-            if (playerHandValue >= 21 || card.card === 'tarot') {
+            if (newHandValue >= 21 || card.card === 'tarot') {
                 setPlayerCanHit(false);
             }
-            //     //lose
-            //     if (gameSavingCards.some(substring => card.valueOfCard.includes(substring))) {
-            //     //player must play cards
-            //     } else {
-            //         handleGameOver({type: "dealer", message: "Player busts, dealer wins"})
-            //     }
-            // } else if (playerHandValue === 21) {
-            //     handleGameOver({type: "player", message: "Player wins"})
-            // }
         }
     };
 
+    // If the dealer can hit they get a card, otherwise the game ends
     const playerStand = () => {
         if (dealerCanHit) {
-            let card = getRandomCard(); 
-            //will need logic for what cards the dealer can and cannot have
-            while (alternateHandCards.some(substring => card.valueOfCard.includes(substring))) {
-                card = getRandomCard();
-            }
+            let card = getRandomCard(true);
             const newDealerHand = [...dealerHand, card];
             const newHandValue = calcHandValue(newDealerHand);
             setDealerHand(newDealerHand);
@@ -96,8 +113,6 @@ export default function Game() {
                 handleGameOver({type: "player", message: "player wins"});
             }
         }
-
-        
     };
 
     const calcHandValue = (hand) => {
@@ -142,43 +157,41 @@ export default function Game() {
         setGameDeck(completeDeck);
     };
 
-    // TODO: this doesn't seem to be working properly
-    // useEffect(()=> {
-    //     if (playerHand.length === 0 && dealerHand === 0) {
-    //         setPlayerHand([getRandomCard(), getRandomCard()]); // need to account for special cards
+    useEffect(()=> {
+        if (playerHand.length === 0 && dealerHand.length === 0) {
+            gameStart();
+        };
 
-    //         setDealerHand([getRandomCard()]); // dealer really needs two cards but one needs to be flipped over
-    //     };
-    //     if (playerHandValue === 21) {
-    //         handleGameOver({type: "player", message: "player wins"});
-    //     } else if (dealerHandValue === 21) {
-    //         handleGameOver({type: "dealer", message: "dealer wins"});
-    //     };
+        // if (playerHandValue === 21) {
+        //     handleGameOver({type: "player", message: "player wins"});
+        // } else if (dealerHandValue === 21) {
+        //     handleGameOver({type: "dealer", message: "dealer wins"});
+        // };
 
-    //     if (gameOver && dealerHand.length <= 5 ){ 
-    //         switch(true) {
-    //             case playerHandValue === 21:
-    //                 setResult({type: "player", message: "player wins"});
-    //                 break;
-    //             case playerHandValue > 21:
-    //                 setResult({type: "dealer", message: "Player busts, dealer wins"});
-    //                 break;
-    //             case dealerHandValue < playerHandValue:
-    //                 playerStand();
-    //                 break;
-    //             case dealerHandValue === playerHandValue && dealerHand.length <= 5:
-    //                 setResult({type: "", message: "draw"});
-    //                 setNewGame(true);
-    //                 break;
-    //             case dealerHandValue > playerHandValue && dealerHandValue <= 21:
-    //                 setResult({type: "dealer", message: "dealer wins"});
-    //                 setNewGame(true);
-    //                 break;
-    //             default:
-    //                 break;
-    //         }
-    //     };
-    // }, [playerHand, dealerHand, gameOver]);
+        // if (gameOver && dealerHand.length <= 5 ){ 
+        //     switch(true) {
+        //         case playerHandValue === 21:
+        //             setResult({type: "player", message: "player wins"});
+        //             break;
+        //         case playerHandValue > 21:
+        //             setResult({type: "dealer", message: "Player busts, dealer wins"});
+        //             break;
+        //         case dealerHandValue < playerHandValue:
+        //             playerStand();
+        //             break;
+        //         case dealerHandValue === playerHandValue && dealerHand.length <= 5:
+        //             setResult({type: "", message: "draw"});
+        //             setNewGame(true);
+        //             break;
+        //         case dealerHandValue > playerHandValue && dealerHandValue <= 21:
+        //             setResult({type: "dealer", message: "dealer wins"});
+        //             setNewGame(true);
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // };
+    }, [playerHand, dealerHand, gameOver]);
 
     return (        
         <>
