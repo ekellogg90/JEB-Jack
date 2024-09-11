efdb35fe37b15763605d20c853b3ecce02f0eb60
Eric <ekellogg90@gmail.com>
2024-09-11 15:16:19 -0500

    made player hand show horizontal

--- client/src/components/Hand.jsx
+++ client/src/components/Hand.jsx
@@ -1,26 +1,47 @@
 import Card from "./Card";
 import cardIMGInfo from '../utils/cardIMGInfo';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function regularHand({cards,owner,handValue}) {
     const cardIMGarr = cardIMGInfo(cards);
     return (
     <div>
         <h2>{owner}: {handValue}</h2>
-        <div>
+        <Container>
+            <Row>
+                {cardIMGarr.map((card, index) => (
+                <Col>
+                <Card 
+                    key={index}
+                    deckName={card.deckName}
+                    cardIndex={card.cardIndex}
+                />
+                </Col>
+            ))}  
+            </Row>
+        </Container>
+
+        {/* <div>
+            <div>
             {cardIMGarr.map((card, index) => (
                 <Card 
                     key={index}
                     deckName={card.deckName}
                     cardIndex={card.cardIndex}
                 />
-            ))}
+            ))} */}
             
             {/*cards.map((card, index) => (
                 <div key={index}>
                     {card.card}:{card.valueOfCard}
                 </div>
             ))*/}
-        </div>
+{/* 
+            </div>
+        </div> */}
     </div>
     );
 }
