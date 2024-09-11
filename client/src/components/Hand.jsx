0d5e4c2843b5c5d41742bdf643c004a9ed66dd26
Ben Hill <benjaminjhill@icloud.com>
2024-09-10 19:37:52 -0500

    added import to hand

--- client/src/components/Hand.jsx
+++ client/src/components/Hand.jsx
@@ -1,4 +1,5 @@
 import Card from "./Card";
+import cardIMGInfo from './utils/cardIMGInfo';
 
 export default function regularHand({cards,owner,handValue}) {
     const cardIMGarr = cardIMGInfo(cards);
