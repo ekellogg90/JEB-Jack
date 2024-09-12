ce5bef62cc004e4680611f92b73eb091e1cae683
Eric <ekellogg90@gmail.com>
2024-09-12 14:30:58 -0500

    JEB

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
     return (
         <footer className="footer container-fluid">
           <a href="https://github.com/JohnKihm" className="text-decoration-none me-5 h6">John Kihm</a>
-          <a href="https://github.com/Ben-jamin05" className="text-decoration-none me-5 h6">Ben Hill</a>
           <a href="https://github.com/ekellogg90" className="text-decoration-none me-5 h6">Eric Kellogg</a>
+          <a href="https://github.com/Ben-jamin05" className="text-decoration-none me-5 h6">Ben Hill</a>
         </footer>
       );
 };
